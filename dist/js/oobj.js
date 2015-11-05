5d8775a1821dc94c165af91c9e5dac1f80a9bb97
Atilla Barros <atilla8huno@gmail.com>
2015-11-05 10:34:48 -0200

    Adicionando nova directiva oobj-fieldset;

--- dist/js/oobj.js
+++ dist/js/oobj.js
@@ -50,6 +50,8 @@ angular.module('oobj-directives.templates', []).run(['$templateCache', function(
     "    </div></script>");
   $templateCache.put("oobj-date-picker/oobj-date-picker.html",
     "<div class=\"form-group form-group-{{inputSize}}\" ng-class=colspan><label ng-show=\"showLabel || label != undefined\"><strong><span ng-bind=label></span></strong> <span class=text-danger ng-show=ngRequired>*</span></label><div class=input-group><input date-range-picker options=opts name={{name}} ng-model=ngModel ng-disabled=ngDisabled ng-required=ngRequired ng-change=onChange($event) ng-blur=onBlur($event) ng-keyup=onKeyup($event) ng-keydown=onKeydown($event) ng-readonly=ngReadonly class=\"form-control date-picker\"><div class=input-group-btn><button type=button class=\"btn btn-default btn-{{inputSize}} oobj-group-input-btn\"><i class=\"fa fa-calendar\"></i></button></div></div></div>");
+  $templateCache.put("oobj-fieldset/oobj-fieldset.html",
+    "<fieldset ng-class=colspan><legend style={{titleStyle}}><span ng-bind=title></span></legend><div ng-transclude></div></fieldset>");
   $templateCache.put("oobj-footer/oobj-footer.html",
     "<footer style=\"background: #6F6F6F;\n" +
     "                  color: #fff;\n" +
@@ -579,6 +581,39 @@ angular.module('oobj-directives.templates', []).run(['$templateCache', function(
         return directive;
     }
 })();
+/**
+ * Created by ATILLA on 05/11/2015.
+ */
+(function () {
+    'use strict';
+
+    angular
+        .module('oobj-directives')
+        .directive('oobjFieldset', oobjFieldset);
+
+    /** @ngInject */
+    function oobjFieldset() {
+        var directive = {
+            link: link,
+            restrict: 'EA',
+            transclude: true,
+            templateUrl: 'oobj-fieldset/oobj-fieldset.html',
+            scope: {
+                id: '@',
+                colspan: '@',
+                title: '@',
+                titleStyle: '@'
+            }
+        };
+
+        return directive;
+
+        function link(scope, element, attrs) {
+
+        }
+    }
+})();
+
 /**
  * Created by Leonardo on 10/5/2015.
  */
