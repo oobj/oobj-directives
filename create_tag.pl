8a01d1ae2dd6f5d2fac10eef2cf5b90cb0a02385
oobj-ci <cd@oobj.com.br>
2015-11-11 08:50:04 -0500

    Commitando script para geração automatizada da tag.

---
+++ create_tag.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+
+use Data::Dumper;
+use strict;      
+use warnings;
+
+# url para requisitar a versão anterior no bower
+my $url = "https://img.shields.io/bower/v/oobj-directives.json";
+
+#requisita a versão anterior no bower
+my $json = `curl $url`;
+
+#checa se o json retornado contem o atributo value
+if($json =~/"value": ?"v(.*)"/) {
+        #quebra o valor do atributo "value" em um array pelos "."
+	my ($major, $minor, $build) = split(/\./, $1);
+        #une novamente incrementando a ultima parte da versão (num do build)
+	my $versao = "v" . join('.', $major, $minor, ++$build);
+
+        commitarVersao($versao);
+} else {
+	print "Impossível determinar versão para gerar a tag.";
+}
+
+sub commitarVersao {
+	my ($versao) = @_;
+	my $apiKey = "4f432841febbad919d9fbb825fc82d4458c8efd4";
+	my $repoUrl = "https://$apiKey\@github.com/oobj/oobj-directives";
+	`git tag $versao -a -m "Gerando versão $versao para publicação no bower."`;
+	`git push --tags $repoUrl`;
+}
+
