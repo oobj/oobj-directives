ae5ef9dbf39d67ae97b6abafbaa10f0b84f8a28a
Atilla Barros <atilla8huno@gmail.com>
2015-11-24 15:43:55 -0200

    Adicionando directiva oobj-multiselect usando bootstrap-select;

---
+++ src/oobj-multiselect/oobj-multiselect.directive.js
@@ -0,0 +1,62 @@
+/**
+ * Created by ATILLA on 24/11/2015.
+ */
+(function() {
+    'use strict';
+
+    angular
+        .module('oobj-directives')
+        .directive('oobjMultiselect', oobjMultiselect);
+
+    /** @ngInject */
+    function oobjMultiselect() {
+        var directive = {
+            require: 'ngModel',
+            restrict: 'EA',
+            templateUrl: 'oobj-multiselect/oobj-multiselect.html',
+            scope: {
+                id: '@',
+                ngModel: '=',
+                colspan: '@',
+                label: '@',
+                showLabel: '=?',
+                ngRequired: '=',
+                onOpen: '&',
+                provider: '=',
+                itemLabel: '@',
+                itemId: '@',
+                inputSize: '@'
+            },
+            compile: compile
+        };
+
+        return directive;
+
+        function compile(tElement, tAttrs) {
+            return {
+                pre: function preLink(scope, element, attrs) {
+                    if (angular.isUndefined(scope.itemLabel)) {
+                        scope.itemLabel = 'descricao';
+                    }
+
+                    if (angular.isUndefined(scope.itemId)) {
+                        scope.itemId = 'id';
+                    }
+
+                    if (angular.isUndefined(scope.inputSize)) {
+                        scope.inputSize = 'sm';
+                    }
+
+                    angular.forEach(scope.provider, function(value) {
+                        $('.selectpicker')
+                            .append($('<option></option>')
+                            .attr('value', value[scope.itemId])
+                            .text(value[scope.itemLabel]));
+                    });
+
+                    $('.selectpicker').selectpicker();
+                }
+            }
+        }
+    }
+})();
