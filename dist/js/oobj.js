3bc5817e089fcb4b777515f738d1b123c6281941
Atilla Barros <atilla8huno@gmail.com>
2015-11-27 17:59:29 -0200

    Atualizando build com novo filtro CPF/CNPJ

--- dist/js/oobj.js
+++ dist/js/oobj.js
@@ -805,6 +805,45 @@ angular.module('oobj-directives.templates', []).run(['$templateCache', function(
 
     angular
         .module('oobj-filters')
+        .filter('cpfCnpj', cpfCnpj);
+
+    /** @ngInject */
+    function cpfCnpj() {
+
+        var cpfCnpj = function (value) {
+            return angular.isUndefined(value) ? value : formattedCPFCNPJ(value);
+        };
+
+        return cpfCnpj;
+
+        function formattedCPFCNPJ(value) {
+            var formatted = value + '';
+            formatted = formatted.replace(/\D/g, '');
+
+            if (formatted.length == 11) {
+                formatted = formatted.replace(/(\d{3})(\d)/, '$1.$2');
+                formatted = formatted.replace(/(\d{3})(\d)/, '$1.$2');
+                formatted = formatted.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+            } else {
+                formatted = formatted.replace(/^(\d{2})(\d)/, '$1.$2');
+                formatted = formatted.replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3');
+                formatted = formatted.replace(/\.(\d{3})(\d)/, '.$1/$2');
+                formatted = formatted.replace(/(\d{4})(\d)/, '$1-$2');
+            }
+
+            return formatted;
+        }
+    }
+})();
+
+/**
+ * Created by ATILLA on 27/11/2015.
+ */
+(function () {
+    'use strict';
+
+    angular
+        .module('oobj-filters')
         .filter('cpf', cpf);
 
     /** @ngInject */
